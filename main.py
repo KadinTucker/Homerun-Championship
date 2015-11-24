c4619c3945114857d88dda177d8a719e9d070d7a
Kxoe <Kxoe>
2015-11-23 19:44:51 -0700

    added several images, edited main.py, and added a sound.

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ This is a program which initializes the library pygame, then runs a loop.
 import pygame
 from pygame.locals import *
 import sys
+    
+def terminate():
+    """
+    Stop all functions of the program.
+    """
+    pygame.quit()
+    sys.exit()
 
 class button():
     """
@@ -12,11 +19,59 @@ class button():
 
     Takes in location, which is the location of the button.
     Takes in button ID, which determines where the button takes you when pressing it.
+    Takes in image, the image in which it displays on the screen.
     """
-    def __init__(self, location, identity):
+    def __init__(self, location, identity, image):
         self.location = location
         self.identity = identity
+        self.image = image
         self.pressed = False
+        
+    def buttonPressed(self):
+        """
+        Checks whether or not a button has been clicked.
+
+        Returns a slightly modified list of buttons, modifying whether or not the button has been pressed.
+        """
+        mouse = pygame.mouse.get_pos()
+        print('CONSOLE: Clicked at point '+str(mouse))
+        if mouse[0] - self.location[0] <= 200 and mouse[0] - self.location[0] >= 0:
+            if mouse[1] - self.location[1] <= 50 and mouse[1] - self.location[1] >= 0:
+                self.pressed = True
+                print('CONSOLE: Button of ID %s has been pressed.' % self.identity)
+
+    def displayButtons(self, display):
+        """
+        Displays the button on the screen as self.image. All button images should be 200px X 50px
+        to have proper dimensions.
+
+        Takes in the main game display screen.
+        """
+        display.blit(self.image, self.location)
+
+def checkButtons(buttons):
+    """
+    Checks whether the ID of all buttons being pressed checks out with any pre-determined functions
+    of specific buttons. If the function goes through and nothing happens, the button becomes un-pressed.
+
+    Takes in the list of all buttons.
+
+    Ex: A 'quit' button will terminate the game when pressed.
+
+    Returns the new game state.
+    """
+    for i in buttons:
+        if i.pressed == True:
+            if i.identity == 'quit':
+                print('CONSOLE: Quit Button pressed.')
+                return 0
+                print('ERROR: Quit not gone through.')
+            if i.identity == 'play':
+                print('CONSOLE: Play button pressed.')
+                return 2
+        else:
+            i.pressed == False
+    return 1
 
 def initButtons():
     """
@@ -25,28 +80,10 @@ def initButtons():
     Returns a list of all buttons in the main menu.
     """
     buttons = []
-    buttons.append(button((250, 200), 'quit'))
-
-def buttonPressed(buttons):
-    """
-    Checks whether or not a button has been clicked.
-    Takes in the list of buttons.
-
-    Returns a slightly modified list of buttons, modifying whether or not the button has been pressed.
-    """
-    mouse = pygame.mouse.get_pos()
-    for i in buttons:
-        if i.location[0] - mouse[0] <= 200 and i.location[0] - mouse[0] >= 0:
-            if i.location[1] - mouse[1] <= 50 and i.location[1] - mouse[1] >= 0:
-                i.pressed = True
+    buttons.append(button((150, 150), 'quit', pygame.image.load('quitbutton.png')))
+    buttons.append(button((100, 20), None, pygame.image.load('title.png')))
+    buttons.append(button((150, 210), 'play', pygame.image.load('playbutton.png')))
     return buttons
-    
-def terminate():
-    """
-    Stop all functions of the program.
-    """
-    pygame.quit()
-    sys.exit()
 
 def initialize():
     """
@@ -56,13 +93,13 @@ def initialize():
     """
     print('CONSOLE: Initializing loop...')
     pygame.init()
-    pygame.display.set_mode((500, 400))
+    display = pygame.display.set_mode((500, 400))
     buttons = initButtons()
     gamestate = 1 #Gamestate 1 is the main menu.
     print('CONSOLE: Init complete. Starting loop.')
-    return buttons, gamestate
+    return buttons, gamestate, display
 
-def runProgram(buttons, gamestate):
+def runProgram(buttons, gamestate, display):
     """
     Run the program main loop
 
@@ -70,19 +107,36 @@ def runProgram(buttons, gamestate):
     """
     while True:
         if gamestate == 1:
+            display.fill((50, 95, 25))
             for event in pygame.event.get():
                 if event.type == MOUSEBUTTONDOWN:
                     if event.button == 1:
-                        buttons = buttonPressed(buttons)
+                        for i in buttons:    
+                            i.buttonPressed()
                 if event.type == QUIT:
-                    terminate()         
+                    terminate()
+            gamestate = checkButtons(buttons)
+            for i in buttons:
+                i.displayButtons(display)
+                
+        elif gamestate == 2:
+            display.fill((30, 75, 5))
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    terminate()
+                    
+        elif gamestate == 0:
+            print('CONSOLE: Quitting game.')
+            terminate()
+
+        pygame.display.update()
 
 def main():
     """
     Initialize data, then run the program.
     """
-    buttons, gamestate = initialize()
-    runProgram(buttons, gamestate)
+    buttons, gamestate, display = initialize()
+    runProgram(buttons, gamestate, display)
 
 if __name__ == '__main__':
     main()
